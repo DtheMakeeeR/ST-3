24161f017d42df6c55fca360df0ffe363aec3076
DtheMakeeeR <danilgol45@mail.ru>
2026-06-05 14:31:56 +0300

    Update TimedDoor.cpp

--- src/TimedDoor.cpp
+++ src/TimedDoor.cpp
@@ -1,9 +1,11 @@
 // Copyright 2021 GHA Test Team
 #include "TimedDoor.h"
-
 #include <stdexcept>
 #include <thread>
 #include <chrono>
+#include <atomic>
+#include <condition_variable>
+#include <mutex>
 
 DoorTimerAdapter::DoorTimerAdapter(TimedDoor& door) : door(door) {}
 
@@ -12,7 +14,7 @@ void DoorTimerAdapter::Timeout() {
 }
 
 TimedDoor::TimedDoor(int timeout)
-    : iTimeout(timeout), isOpened(false) {
+    : iTimeout(timeout), isOpened(false), adapter(nullptr) {
     adapter = new DoorTimerAdapter(*this);
 }
 
@@ -28,24 +30,41 @@ void TimedDoor::lock() {
     isOpened = false;
 }
 
-int TimedDoor::getTimeOut() const {
-    return iTimeout;
-}
-
 void TimedDoor::throwState() {
     if (isOpened) {
-        throw std::runtime_error("Door has been opened for too long!");
+        throw std::runtime_error("Door timeout: door was left open too long!");
     }
 }
 
-void Timer::sleep(int t) {
-    std::this_thread::sleep_for(std::chrono::milliseconds(t));
+void Timer::sleep(int seconds) {
+    auto start = std::chrono::steady_clock::now();
+    auto duration = std::chrono::seconds(seconds);
+
+    while (std::chrono::steady_clock::now() - start < duration) {
+        std::this_thread::yield();
+    }
 }
 
 void Timer::tregister(int timeout, TimerClient* client) {
     if (client == nullptr) {
         return;
     }
-    sleep(timeout);
+
+    this->client = client;
+
+    if (timeout <= 0) {
+        client->Timeout();
+        return;
+    }
+
+    int waitTimeMs = timeout;
+
+    auto startTime = std::chrono::steady_clock::now();
+    auto waitDuration = std::chrono::milliseconds(waitTimeMs);
+
+    while (std::chrono::steady_clock::now() - startTime < waitDuration) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    }
+
     client->Timeout();
 }
